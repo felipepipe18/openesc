d541de5ccd85bc4165222a4204e4058aecf44359
Jason R. Jones <slightlynybbled@gmail.com>
2012-09-12 20:15:31 -0400

    Added OpenESC headers just to software to start fleshing out file structure.

--- software/main.c
+++ software/main.c
@@ -1,6 +1,36 @@
+/**************************************************
+ * Copyright 2012 OpenESC
+ *
+ * This file is part of OpenESC
+ *
+ * OpenESC is free software: you can redistribute
+ *	it and/or modify it under the terms of the GNU
+ *	General Public License as published by the
+ *	Free Software Foundation, either version 3 of
+ *	the License, or (at your option) any later
+ *	version.
+ *
+ * OpenESC is distributed in the hope that it will
+ *	be useful, but WITHOUT ANY WARRANTY; without
+ *	even the implied warranty of MERCHANTABILITY
+ *	or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *	GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU
+ *	General Public License along with OpenESC. If
+ *	not, see http://www.gnu.org/licenses/.
+*************************************************/
+
+#include "openesc.h"
 
 int main(void)
 {
+	// Setup all of the pins and peripherals
+	//	of the openESC board
+	// setupOpenESC();
+
+	// Run the bootloader
+	// bootloader();
 
     while(1)
     {
---
+++ software/openesc_v01.h
@@ -0,0 +1,62 @@
+/**************************************************
+ * Copyright 2012 OpenESC
+ *
+ * This file is part of OpenESC
+ *
+ * OpenESC is free software: you can redistribute
+ *	it and/or modify it under the terms of the GNU
+ *	General Public License as published by the
+ *	Free Software Foundation, either version 3 of
+ *	the License, or (at your option) any later
+ *	version.
+ *
+ * OpenESC is distributed in the hope that it will
+ *	be useful, but WITHOUT ANY WARRANTY; without
+ *	even the implied warranty of MERCHANTABILITY
+ *	or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *	GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU
+ *	General Public License along with OpenESC. If
+ *	not, see http://www.gnu.org/licenses/.
+*************************************************/
+
+/**************************************************
+ * Purpose: To define all of the pins in an easily
+ *	human-readable format.
+ *
+ * Usage: This file should be included in "openesc.h"
+*************************************************/
+
+#ifndef OPENESC_V01
+#define OPENESC_V01
+
+// Define ADC pins
+
+
+
+// Define RC PWM pins
+
+
+
+// Define motor control PWM pins
+
+
+
+// Define USART pins
+
+
+
+// Define USB pins
+
+
+
+// Define I2C pins
+
+
+
+// Define LED pins
+
+
+
+#endif
